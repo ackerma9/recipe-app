eba6d8f5ebce514882064238a029d3b1a725597f
Brian <backermann92@gmail.com>
2020-05-04 12:53:40 -0500

    completing assignment.

---
+++ src/main/java/guru/springframework/recipe/domain/Notes.java
@@ -0,0 +1,33 @@
+package guru.springframework.recipe.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNote;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRecipeNote() {
+        return recipeNote;
+    }
+
+    public void setRecipeNote(String recipeNote) {
+        this.recipeNote = recipeNote;
+    }
+}
