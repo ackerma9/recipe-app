67b3f82a96828c6153320f40c41209617036078a
Brian <backermann92@gmail.com>
2020-05-08 13:55:22 -0500

    completing assignment.

---
+++ src/main/java/guru/springframework/recipe/repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,14 @@
+package guru.springframework.recipe.repositories;
+
+import guru.springframework.recipe.domain.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+/**
+ * Created by jt on 6/13/17.
+ */
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
+}
